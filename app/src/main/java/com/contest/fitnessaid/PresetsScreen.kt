5bdc381ac1f7f595fee698316d361b873a878d4d
MadRat-0 <adritomukherjee@gmail.com>
2022-01-06 23:34:35 +0530

    preset recycler view done!

--- app/src/main/java/com/contest/fitnessaid/PresetsScreen.kt
+++ app/src/main/java/com/contest/fitnessaid/PresetsScreen.kt
@@ -5,11 +5,23 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class PresetsScreen : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_presets_screen)
+
+        val  recyclerView=findViewById<RecyclerView>(R.id.preset_view)
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        adapter=RecyclerAdapter()
+        recyclerView.adapter=adapter
 
         val sharedPreferences: SharedPreferences = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
         val username = sharedPreferences.getString("username", " ")
--- app/src/main/java/com/contest/fitnessaid/RecyclerAdapter.kt
+++ app/src/main/java/com/contest/fitnessaid/RecyclerAdapter.kt
@@ -1,23 +1,40 @@
 package com.contest.fitnessaid
 
+import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private var titles = arrayOf("Chest", "Back", "Biceps", "Triceps", "Leg", "Cardio")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.presets_card_layout, parent, false)
+        return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.presetTitle.text = titles[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return titles.size
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        var presetTitle : TextView
+        var activitiesIn : TextView
+        var viewButton : Button
+
+        init {
+            presetTitle = itemView.findViewById(R.id.preset_title)
+            activitiesIn = itemView.findViewById(R.id.activities_in)
+            viewButton = itemView.findViewById(R.id.view_button)
+        }
 
     }
 
