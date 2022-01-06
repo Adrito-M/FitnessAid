70a9b5ee4340cfa39d35f14e0c9d210fc24a8ca4
somudas <soumyadeep4122002@gmail.com>
2022-01-06 09:34:02 +0530

    added the splash screen minor changes in android manifest
    MainActivity is the Splash Screen
    Screen2 is the primary screen.

---
+++ app/src/main/java/com/contest/fitnessaid/Screen2.kt
@@ -0,0 +1,11 @@
+package com.contest.fitnessaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Screen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen2)
+    }
+}
